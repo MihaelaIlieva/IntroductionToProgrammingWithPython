9f520e4f6f7c14025ace2ea5b66cb32d82f0d1ab
Mihaela Ilieva <mih1711@abv.bg>
2023-10-27 16:12:38 +0300

    Added decorators exercise another example

--- DecoratorsRestaurantExercise.py
+++ DecoratorsRestaurantExercise.py
@@ -26,3 +26,16 @@ eggs=thank_you(served_by(eggs, "sir"))
 spam=thank_you(served_by(spam, "sir"))
 print(eggs(10))
 print(spam(10))
+
+def food(type_of_food, count_of_food):
+    return "I would like {} servings of {}".format(count_of_food,type_of_food)
+def served_by(function, server):
+    def cached_server(type_of_food, count_of_food):
+        return "{}, dear {}.".format(function(type_of_food, count_of_food),server)
+    return cached_server
+def thank_you(function):
+    def with_thanks(type_of_food, count_of_food):
+        return "{} Thank you very much!".format(function(type_of_food, count_of_food))
+    return with_thanks
+portion=thank_you(served_by(food,"sir"))
+print(portion("eggs", 15))
